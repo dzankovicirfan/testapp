8d7c38249c2e5f96678722c5e6bbd7b516d5b01d
Irfan <dzankovicirfan@gmai.com>
2018-02-10 12:33:57 +0100

    gitignore i date

--- testapp/settings.py
+++ testapp/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 
 
 REST_FRAMEWORK = {
-    'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S',
+    'DATE_FORMAT': '%d.%m.%Y',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
