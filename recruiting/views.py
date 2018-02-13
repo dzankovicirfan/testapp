2ad8092184529fc58e15792764deff2d0505c954
DESKTOP-VT6V30U\irfan <dzankovicirfan@gmail.com>
2018-02-13 15:32:28 +0100

    authentication is staff user

--- recruiting/views.py
+++ recruiting/views.py
@@ -3,11 +3,18 @@ from __future__ import unicode_literals
 
 from rest_framework.viewsets import ModelViewSet
 
+import django_filters.rest_framework
+from rest_framework.permissions import IsAdminUser
+
 from recruiting.models import Vacancy
 from recruiting.serializers import VacancySerializer
+
 
 
 class VacancyView(ModelViewSet):
     queryset = Vacancy.objects.is_active()
     serializer_class = VacancySerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
     filter_field = ('location', 'company')
+    permisssion_classes = (IsAdminUser ,)
+
